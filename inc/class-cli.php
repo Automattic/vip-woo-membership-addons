11c356acfb9a54ffb5d9918d234a025eb3352577
Rick Hurst <rick.hurst@automattic.com>
2023-04-10 16:42:17 +0100

    Removed generateusers command to avoid confusion/ accidental usage on prod

--- inc/class-cli.php
+++ inc/class-cli.php
@@ -3,54 +3,6 @@
 class VIP_Woo_Membership_CLI {
 
     /**
-     * This is for generating users locally for testing only
-     */
-
-    // Usage: wp vip-woo-membership generateusers
-    public function generateusers() {
-        WP_CLI::log( 'generating users' );
-
-        $required_user_count = 8000;
-        $user_membership = 12;
-
-        $prefix = base64_encode(random_bytes(3));
-
-        $progress = \WP_CLI\Utils\make_progress_bar( 'Generating Posts', $required_user_count );
-
-        for( $i=0; $i < $required_user_count; $i++ ) {
-
-            $username = 'member' . $prefix . $i+1;
-
-            $user_id = wp_insert_user( [
-                'user_login' => $username,
-                'user_pass' => md5( $username ),
-                'user_email' => $username .'@example.com',
-                'first_name' => $username,
-                'last_name' => 'Lastname',
-                'display_name' => $username,
-                'role' => 'customer'
-            ] );
-
-            // add a user membership
-            $membership_post = [
-                'post_type' => 'wc_user_membership',
-                'post_status' =>  'wcm-active',
-                'post_name' => $username,
-                'post_parent' => $user_membership,
-                'post_author' => $user_id,
-            ];
-    
-            // update/insert post 
-            $post_id = wp_insert_post( $membership_post );
-
-            $progress->tick();
-
-        }
-
-        $progress->finish();
-    }
-
-    /**
      * Export subscribers
      *
      * ## OPTIONS
